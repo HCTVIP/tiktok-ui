319594b5df6a0ea7f09d0b47278c77950d95992a
HCTVIP <108228025+HCTVIP@users.noreply.github.com>
2022-09-29 22:16:53 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <span>may ac qua github a</span>
-=======
       <span>Hello người anh em github</span>
->>>>>>> 389c9fbad6b4d9fd2fcad52dccdae381a6c72002
     </div>
   );
 }
