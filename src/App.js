6a3912b1cba357f1c48eb1a11194a787d0d39ccd
Teo <teo@gmail.com>
2022-10-01 12:45:47 +0700

    update ngày 1/10/2022

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './component/Layout';
 function App() {
-  return (
-    <div className="App">
-      <span>Hello người anh em github</span>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
